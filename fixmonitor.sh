1fea7fe6f0ea0a35b00414155bdb92223c97aaad
xji999 <xji999@gmail.com>
2018-06-27 09:19:34 -0400

    script to autofix resolv.conf

---
+++ fixmonitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fix lack of openshift node NM was not enabled in vagrant/virtualbox env
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+while [ ! -d "/etc/origin/node" ]
+do
+  echo "no such dir yet"
+  sleep 5
+done
+
+touch /etc/origin/node/resolv.conf
